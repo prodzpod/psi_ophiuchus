c16959300a7f3a26ff78a94b85d183ad3f0c491a
prodzpod <product0339@gmail.com>
2020-06-15 23:44:52 +0900

    i forgot to commit sorry everyone

---
+++ src/main/java/kr/o_r/prodzpod/ophiuchus/util/ThisMod.java
@@ -0,0 +1,9 @@
+package kr.o_r.prodzpod.ophiuchus.util;
+
+public class ThisMod {
+    public static final String ID = "ophiuchus";
+    public static final String NAME = "Psi: Ophiuchus";
+    public static final String VERSION = "0.1.69";
+    public static final String PREFIX = ID + ":";
+    public static final boolean DEBUG = true; // FALSE THIS AFTER LIB IS SET UP
+}
---
+++ template/ThisMod.java
@@ -0,0 +1,9 @@
+package kr.o_r.prodzpod.ophiuchus.util;
+
+public class ThisMod {
+    public static final String ID = "%MODID%";
+    public static final String NAME = "%MODNAME%";
+    public static final String VERSION = "%VERSION%";
+    public static final String PREFIX = ID + ":";
+    public static final boolean DEBUG = true; // FALSE THIS AFTER LIB IS SET UP
+}
